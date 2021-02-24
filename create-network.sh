aa50f65a78aec13a4ce83d8ff6a6a7ad363658d6
kien <kienle371999@gmail.com>
2021-02-24 07:24:47 +0700

    oki

--- create-network.sh
+++ create-network.sh
@@ -13,7 +13,7 @@ cd $mydir/
 ./clean.sh $PROJECT
 
 # Create new namspace
-kubectl create namespace "$PROJECT-namespace"
+# kubectl create namespace "$PROJECT-namespace"
 
 PROJECT_FOLDER=$PWD/networks/$PROJECT
 
@@ -25,10 +25,14 @@ helm template config-templates/ -s templates/crypto-config.yaml -f $CONFIG_FILE 
 helm template config-templates/ -s templates/hlf-kube-value.yaml -f $CONFIG_FILE | sed -n '1!p'  > $PROJECT_FOLDER/hlf-kube-value.yaml
 helm template config-templates/ -s templates/network.yaml | sed -n '1!p'  > $PROJECT_FOLDER/network.yaml
 
+# ./init.sh $PROJECT_FOLDER
+
+./init.sh $PROJECT_FOLDER
+
 echo "Start creating network..."
 # helm template hlf-kube/ -s templates/ca-deployment.yaml -f $PROJECT_FOLDER/network.yaml -f $PROJECT_FOLDER/crypto-config.yaml --values=$PROJECT_FOLDER/hlf-kube-value.yaml  | sed -n '1!p' > $PROJECT_FOLDER/ca222.yaml 
 
-helm install $PROJECT ./hlf-kube -f $PROJECT_FOLDER/network.yaml -f $PROJECT_FOLDER/crypto-config.yaml --values=$PROJECT_FOLDER/hlf-kube-value.yaml -n "$PROJECT-namespace" --wait 
+helm install $PROJECT ./hlf-kube -f $PROJECT_FOLDER/network.yaml -f $PROJECT_FOLDER/crypto-config.yaml --values=$PROJECT_FOLDER/hlf-kube-value.yaml --wait 
 ./collect_host_aliases.sh $PROJECT_FOLDER/
 echo "Updating host aliases..."
-helm upgrade $PROJECT ./hlf-kube -f $PROJECT_FOLDER/network.yaml -f $PROJECT_FOLDER/crypto-config.yaml -f $PROJECT_FOLDER/hostAliases.yaml -n "$PROJECT-namespace" --values=$PROJECT_FOLDER/hlf-kube-value.yaml --wait 
+helm upgrade $PROJECT ./hlf-kube -f $PROJECT_FOLDER/network.yaml -f $PROJECT_FOLDER/crypto-config.yaml -f $PROJECT_FOLDER/hostAliases.yaml --values=$PROJECT_FOLDER/hlf-kube-value.yaml --wait 
--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-console.log(typeof [])
+var x = { a: 5 }
+var channel = [13, 15, 14]
+
+console.log({ x, channel })
